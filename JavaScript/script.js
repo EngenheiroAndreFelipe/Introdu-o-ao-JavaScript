08a424681ac0cd16e7908a169f2394cd624feaef
Andre Felipe <andrefelipearacaju@gmail.com>
2022-06-28 07:55:27 -0300

    Initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,48 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+}
+/* function clicou(){
+    alert("Obrigado por clicar")
+}
+
+
+/*function soma(a, b){
+	return a + b;
+}
+
+var validar;
+function validaIdade(idade){
+   
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+validaIdade(idade);
+console.log(validar);
+//alert(soma(20, 30));
+//console.log(5, 10);
+//var d = new Date();
+//alert(d);
+
+//var fruta = {nome:"maçã", cor:"vermelha"}
+//console.log(fruta);
+
+//construção de dicionário;
+/*var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva")
+console.log(lista);
+var nome = "André Felipe.";
+var idade = 33;
+var idade2 = 15;
+var frase = "Japão é a melhor seleção do mundo."
+//alert("Vá tomar no cu, " + nome);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"))
+*/
